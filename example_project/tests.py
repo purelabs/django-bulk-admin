ee5ffa5712f6750f3e8a3dd37624582836e530f7
Y. Wayne Huang <wayne530@gmail.com>
2019-01-13 13:24:34 -0800

    fix deprecation warning and import reverse from django.urls

--- example_project/tests.py
+++ example_project/tests.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from django.contrib.admin.sites import site as admin_site
 from django.contrib.auth.models import Permission, User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils import six
 from io import BytesIO
 
